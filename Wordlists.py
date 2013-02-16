6efcc0daece96c334be91665d0e5ea383a523df8
Konstantin Martini <k@tuxcode.org>
2013-02-16 07:47:35 +0100

    included cmavo.txt

--- Wordlists.py
+++ Wordlists.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 """ A parser for the official wordlists. """
 
+valsi_dict = {} # the current purpose of the module
+
 import os
 
 # as packaged in debian
 folder = os.path.sep.join(("", "usr", "share", "lojban"))
+
+##
+#  gismu.txt
+##
+
 gismu_file = os.path.sep.join((folder, "gismu.txt"))
 
 print "Reading gismu file: %s" % gismu_file
@@ -12,8 +19,6 @@ gismu_unparsed = [ line.strip() for line in open(gismu_file, "r") ]
 
 from collections import namedtuple
 Gismu = namedtuple("Gismu", ["valsi", "rafsi", "gloss", "strange", "trans", "other"]) # to be continued...
-
-valsi_dict = {}
 
 def parse_gismu(line):
 
@@ -46,10 +51,46 @@ print "Gismu file version: %s" % gismu_version.split()[0]
 gismu = map(parse_gismu, gismu_unparsed)
 
 
+
+##
+#  cmavo.txt
+##
+
+cmavo_file = os.path.sep.join((folder, "cmavo.txt"))
+
+print "Reading cmavo file: %s" % cmavo_file
+cmavo_unparsed = [ line.strip() for line in open(cmavo_file, "r") ]
+
+from collections import namedtuple
+Cmavo = namedtuple("Cmavo", ["valsi", "clas", "gloss", "trans"])
+
+def parse_cmavo(line):
+
+    valsi = line[:10].strip()
+    line = line[10:]
+
+    clas = line[:9].strip().split()
+    line = line[9:]
+
+    gloss = line[:42].strip()
+    line = line[42:]
+
+    trans = line.strip()
+
+    cmavo = Cmavo(valsi, clas, gloss, trans)
+
+    valsi_dict[valsi] = cmavo
+    return cmavo
+
+
+cmavo_version = cmavo_unparsed.pop(0).strip()
+print "Cmavo file version: %s" % cmavo_version.split()[0]
+
+cmavo = map(parse_cmavo, cmavo_unparsed)
 if __name__ == "__main__":
 
-    for idx, g in enumerate(gismu):
-        print "%4d: %-5s - %-12s - %s" % (idx, g.valsi, " ".join(g.rafsi), g.trans)
+    for idx, g in enumerate(cmavo):
+        print "%4d: %-5s - %-12s - %s" % (idx, g.valsi, g.gloss, g.trans)
 
 
 
